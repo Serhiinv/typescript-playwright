7593a3eb8bc358cd80f326f2ef8148d23719ba50
sn <serhii.nv@gmail.com>
2025-12-22 12:50:48 +0000

    f

---
+++ page-object/users.ts
@@ -0,0 +1,26 @@
+
+export class Users {
+    // L&T Staging
+    static readonly user_lnt_stg = process.env.USER_LNT_STG_ID || '';
+    static readonly url_lnt_stg = process.env.USER_LNT_STG_URL || '';
+    static readonly domain_lnt_stg = process.env.USER_LNT_STG_DOMAIN || '';
+    // L&T Production
+    static readonly user_lnt = process.env.USER_LNT_ID || '';
+    static readonly url_lnt = process.env.USER_LNT_URL || 'https://www.lyonandturnbull.com/';
+    static readonly domain_lnt = process.env.USER_LNT_DOMAIN || 'www.lyonandturnbull.com/';
+
+    // SGB Staging
+    static readonly user_sgb_stg = process.env.USER_SGB_STG_ID || '';
+    static readonly url_sgb_stg = process.env.USER_SGB_STG_URL || '';
+    static readonly domain_sgb_stg = process.env.USER_SGB_STG_DOMAIN || '';
+
+    // SGB Production
+    static readonly vercel_jwt = process.env.VERCEL_JWT || '';
+    static readonly vercel_uuid = process.env.VERCEL_UUID || '';
+    static readonly vercel_domain = process.env.VERCEL_DOMAIN || '';
+
+    static readonly user_sgb = process.env.USER_SGB_ID || '';
+    static readonly url_sgb = process.env.USER_SGB_URL || '';
+    static readonly domain_sgb = process.env.USER_SGB_DOMAIN || '';
+}
+
