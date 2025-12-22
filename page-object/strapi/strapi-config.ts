481ecd77a7b4071808bc554f6ea9a9a4aac1f6ab
sn <serhii.nv@gmail.com>
2025-12-22 13:02:11 +0000

    f

---
+++ page-object/strapi/strapi-config.ts
@@ -0,0 +1,6 @@
+
+export const StrapiConfig = {
+    email: process.env.STRAPI_EMAIL || '',
+    password: process.env.STRAPI_PASSWORD || '',
+    loginUrl: 'https://integral-cow-029df26137.strapiapp.com/admin/auth/login'
+};
