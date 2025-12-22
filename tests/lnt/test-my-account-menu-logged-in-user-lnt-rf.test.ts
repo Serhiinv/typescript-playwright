d2c4bfefb800c873d52ad3d879f86ce4d4616bf5
sn <serhii.nv@gmail.com>
2025-12-22 15:35:06 +0000

    f

--- tests/lnt/test-my-account-menu-logged-in-user-lnt-rf.test.ts
+++ tests/lnt/test-my-account-menu-logged-in-user-lnt-rf.test.ts
@@ -1,6 +1,8 @@
 import { test, expect, Page } from '@playwright/test';
 import { UserPage } from '../../page-object/lnt/user-page';
 import { Users } from '../../page-object/users';
+import allure from 'allure-js-commons';
+
 
 async function verifyPage(page: Page): Promise<void> {
     await expect(page.getByText("Oops!")).not.toBeVisible({ timeout: 3000 });
@@ -29,6 +31,7 @@ async function clickOnMenu(page: Page, menuName: string): Promise<void> {
 // });
 
 test('test my account menu logged in user REFACTORED @lnt-menu-logged', async ({ page }) => {
+    await allure.issue("JIRA-test name", "https://jira.test/browse/JIRA-test-name");
     const up = new UserPage(page);
     await up.open(Users.user_lnt, Users.url_lnt, Users.domain_lnt);
 
