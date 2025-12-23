26d0d9bda769bd0b91f4735b3bb6d301b1280fed
sn <serhii.nv@gmail.com>
2025-12-23 08:53:28 +0000

    lnt-color-scheme test.step

--- tests/lnt/strapi/strapi-lnt-colour-scheme-selection-on-component.test.ts
+++ tests/lnt/strapi/strapi-lnt-colour-scheme-selection-on-component.test.ts
@@ -1,7 +1,9 @@
 import { test } from '@playwright/test';
 import {StrapiHelper} from "../../../page-object/strapi/strapi-helper";
+import * as allure from "allure-js-commons";
 
 test('Strapi - Test colour scheme selection on component @lnt-color-scheme', async ({ page }) => {
+  await allure.issue("JIRA-test name", "https://jira.test/browse/JIRA-test-name");
 
   const strapi = new StrapiHelper(page);
   const slug = 'sn-page-at';
