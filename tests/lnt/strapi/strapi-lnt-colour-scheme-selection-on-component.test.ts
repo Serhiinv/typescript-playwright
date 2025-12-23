62f466532488b7d8ab25d418b645689d366e497b
sn <serhii.nv@gmail.com>
2025-12-23 22:02:07 +0000

    f

--- tests/lnt/strapi/strapi-lnt-colour-scheme-selection-on-component.test.ts
+++ tests/lnt/strapi/strapi-lnt-colour-scheme-selection-on-component.test.ts
@@ -2,8 +2,8 @@ import { test } from '@playwright/test';
 import {StrapiHelper} from "../../../page-object/strapi/strapi-helper";
 import * as allure from "allure-js-commons";
 
-import { UserPage } from '../../../page-object/lnt/user-page';
-import { Users } from '../../../page-object/users';
+// import { UserPage } from '../../../page-object/lnt/user-page';
+// import { Users } from '../../../page-object/users';
 
 test('Strapi - Test colour scheme selection on component @lnt-color-scheme', async ({ page }) => {
   await allure.issue("JIRA-test name", "https://jira.test/browse/JIRA-test-name");
@@ -12,9 +12,6 @@ test('Strapi - Test colour scheme selection on component @lnt-color-scheme', asy
   const slug = 'sn-page-at';
 
   await strapi.login(); //not secure
-
-  // const up = new UserPage(page); //secure
-  // await up.openStrapi(Users.strapi_lnt_jwt, Users.strapi_lnt_domain);
 
   await strapi.navigateToPages();
   await strapi.skipTutorial();
@@ -26,3 +23,10 @@ test('Strapi - Test colour scheme selection on component @lnt-color-scheme', asy
   await strapi.publishPage();
   await strapi.verifyArtistComponentBackground(bgColor);
 });
+
+
+// const up = new UserPage(page); //secure
+// await up.openStrapi(Users.strapi_lnt_jwt, Users.strapi_lnt_domain);
+
+// STRAPI_LNT_JVT=
+// STRAPI_LNT_DOMAIN=
