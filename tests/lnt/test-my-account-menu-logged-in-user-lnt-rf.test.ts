ff0eb02f36e0fe5edc0c854f0c47f574b02c29f6
sn <serhii.nv@gmail.com>
2025-12-25 17:01:18 +0000

    No &

--- tests/lnt/test-my-account-menu-logged-in-user-lnt-rf.test.ts
+++ tests/lnt/test-my-account-menu-logged-in-user-lnt-rf.test.ts
@@ -32,8 +32,8 @@ async function clickOnMenu(page: Page, menuName: string): Promise<void> {
 // });
 const runTag = process.env.RUN_TAG || 'all';
 
-test('L&T - Test my account menu logged in user @L&T-menu', async ({ page }) => {
-    test.skip(runTag !== 'all' && runTag !== '@L&T-menu', 'Not running smoke tests');
+test('lnt - Test my account menu logged in user @lnt-menu', async ({ page }) => {
+    test.skip(runTag !== 'all' && runTag !== '@lnt-menu', 'Not running smoke tests');
 
     await allure.issue("JIRA-test name", "https://jira.test/browse/JIRA-test-name");
     const up = new UserPage(page);
