d70d8cf15a561298fe6b5f97d818ac60af5136f9
sn <serhii.nv@gmail.com>
2025-12-26 19:18:57 +0000

    env var + strapi

--- page-object/strapi/strapi-config.ts
+++ page-object/strapi/strapi-config.ts
@@ -1,6 +1,16 @@
 
-export const StrapiConfig = {
-    email: process.env.STRAPI_EMAIL || '',
-    password: process.env.STRAPI_PASSWORD || '',
-    loginUrl: 'https://integral-cow-029df26137.strapiapp.com/admin/auth/login'
+export const strapi = {
+    lnt_stg: {
+        email: process.env.STRAPI_EMAIL || '',
+        password: process.env.STRAPI_PASSWORD || '',
+        loginUrl: 'https://integral-cow-029df26137.strapiapp.com/admin/auth/login'
+    },
+    sgb_stg: {
+        email: process.env.STRAPI_EMAIL_SGB || '',
+        password: process.env.STRAPI_PASSWORD_SGB || '',
+        loginUrl: 'https://integr26137.strapiapp.com/admin/auth/login' //TODO
+    },
 };
+
+const environment = (process.env.TEST_ENV || 'lnt_stg') as 'lnt_stg' | 'sgb_stg';
+export const StrapiConfig = strapi[environment];
