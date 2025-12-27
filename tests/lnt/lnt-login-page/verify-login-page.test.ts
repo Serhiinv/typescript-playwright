9d13e13a43b247bc7ac32e6090770192f8b1cc76
sn <serhii.nv@gmail.com>
2025-12-27 12:24:02 +0000

    f

--- tests/lnt/lnt-login-page/verify-login-page.test.ts
+++ tests/lnt/lnt-login-page/verify-login-page.test.ts
@@ -1,33 +1,41 @@
-import { test, expect } from "@playwright/test";
-import { LNTCreateAccountPage } from "../../../page-object/lnt/lnt-pages/LNTCreateAccountPage";
-import { LNTLoginPage } from "../../../page-object/lnt/lnt-pages/LNTLoginPage";
-import { LNTHomePage } from "../../../page-object/lnt/lnt-pages/LNTHomePage";
-import { LNTHeaderMenuPage } from "../../../page-object/lnt/lnt-pages/LNTHeaderMenuPage";
+import {test, expect} from "@playwright/test";
+import {LNTCreateAccountPage} from "../../../page-object/lnt/lnt-pages/LNTCreateAccountPage";
+import {LNTLoginPage} from "../../../page-object/lnt/lnt-pages/LNTLoginPage";
+import {LNTHomePage} from "../../../page-object/lnt/lnt-pages/LNTHomePage";
+import {LNTHeaderMenuPage} from "../../../page-object/lnt/lnt-pages/LNTHeaderMenuPage";
 import * as allure from "allure-js-commons";
 
 const runTag = process.env.RUN_TAG || 'all';
+// const testName = 'tests Verify Login Page';
 
-test('tests Verify Login Page @smoke', async ({ page }) => {
-  test.skip(runTag !== 'all' && runTag !== '@smoke' && runTag !== 'tests Verify Login Page', 'Not runed');
-  await allure.tag(`Environment: ${process.env.TEST_ENV || 'Local'}`)
-  await allure.historyId(`${process.env.TEST_ENV || 'Local'}`)
+test('tests Verify Login Page @smoke', async ({page}) => {
+    test.skip(runTag !== 'all' && runTag !== '@smoke' && runTag !== 'tests Verify Login Page', 'Not runed');
+    await allure.tag(`Environment: ${process.env.TEST_ENV || 'Local'}`)
+    await allure.historyId('lnt-login-page-verify')
+    await allure.parameter('Environment', process.env.TEST_ENV || 'Local')
+
+// test(`${testName} @smoke`, async ({page}) => {
+//     test.skip(runTag !== 'all' && runTag !== '@smoke' && runTag !== `${testName}`, 'Not runed');
+//     await allure.tag(`Environment: ${process.env.TEST_ENV || 'Local'}`)
+//     await allure.historyId(`${testName}`)
+//     await allure.parameter('Environment', process.env.TEST_ENV || 'Local')
 
 
-  await new LNTHomePage(page).open();
-  // const headerMenu = new LNTHeaderMenuPage(page);
-  // await headerMenu.logIn();
-  // await headerMenu.signIn();
-  //
-  // // const createAccount = new LNTCreateAccountPage(page);
-  // const loginPage = new LNTLoginPage(page);
+    await new LNTHomePage(page).open();
+    // const headerMenu = new LNTHeaderMenuPage(page);
+    // await headerMenu.logIn();
+    // await headerMenu.signIn();
+    //
+    // // const createAccount = new LNTCreateAccountPage(page);
+    // const loginPage = new LNTLoginPage(page);
 
-  // await loginPage.verifyHeader();
-  // await loginPage.verifyTextDescription1();
-  // await loginPage.verifyTextDescription2();
-  //
-  // // Using methods from CreateAccountPage as there are the same elements
-  // await createAccount.verifyGoogleLogin();
-  // await createAccount.verifyAppleLogin();
-  // // await createAccount.verifyFacebookLogin();
-  // await createAccount.verifyEmailLogin("Login");
+    // await loginPage.verifyHeader();
+    // await loginPage.verifyTextDescription1();
+    // await loginPage.verifyTextDescription2();
+    //
+    // // Using methods from CreateAccountPage as there are the same elements
+    // await createAccount.verifyGoogleLogin();
+    // await createAccount.verifyAppleLogin();
+    // // await createAccount.verifyFacebookLogin();
+    // await createAccount.verifyEmailLogin("Login");
 });
