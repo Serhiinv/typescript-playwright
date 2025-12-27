fe9487b3ec1b48edbb7963bac0d41018051b35e9
sn <serhii.nv@gmail.com>
2025-12-27 11:28:05 +0000

    f

--- tests/lnt/strapi/strapi-lnt-colour-scheme-selection-on-component.test.ts
+++ tests/lnt/strapi/strapi-lnt-colour-scheme-selection-on-component.test.ts
@@ -6,7 +6,8 @@ const runTag = process.env.RUN_TAG || 'all';
 
 test('lnt strapi - Test colour scheme selection on component @lnt-strapi', async ({page}) => {
     test.skip(runTag !== 'all' && runTag !== '@lnt-strapi' && runTag !== 'lnt strapi - Test colour scheme selection', 'Not runed');
-    await allure.issue("JIRA-test name", "https://jira.test/browse/JIRA-test-name");
+    await allure.tag(`Environment: ${process.env.TEST_ENV || 'Local'}`)
+    await allure.issue( "https://binary-vision.atlassian.net/browse/AUC-2596", "Strapi regular updates");
 
     const strapi = new StrapiHelper(page);
     const slug = 'sn-page-at';
